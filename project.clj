315fd65c7c4ea43dcda2ca9bb88fcbc570b0c9da
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-24 22:23:24 +0100

    Sort Dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[clj-time "0.11.0"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [clj-time "0.11.0"]
                  [prismatic/plumbing "0.5.2"]
                  [prismatic/schema "1.0.4"]]
 
